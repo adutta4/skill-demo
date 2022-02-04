7b0966b500ee6966ac1b748ea649d9497e387858
adutta4 <duttaaditi37@gmail.com>
2022-02-03 21:44:50 -0800

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,5 +1,8 @@
 public class Example {
     public static String firstWord(String str) {
+        if(str.indexOf(" ") == -1) {
+            return str;
+        }
         return str.substring(0, str.indexOf(" "));
     }
 }
